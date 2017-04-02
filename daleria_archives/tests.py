7fe03e59b01cd7a2cdc0fbf1e9f8e7c9cb2efeee
mhhoban <mhhoban@gmail.com>
2017-04-01 17:29:43 -0700

    added unit test to ensure db content being correctly served, renamed 'full_list' view to more descriptive 'full_list_page'

--- daleria_archives/tests.py
+++ daleria_archives/tests.py
@@ -1,12 +1,58 @@
-from daleria_archives.views import home_page
-
+from daleria_archives.views import home_page, full_list_page
+from daleria_archives.models import Card
+from django.core import serializers
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+import HTMLParser
 
-class PageLoadsTest(TestCase):
+class HomePageLoadsTest(TestCase):
 
     def test_archive_homepage_loads(self):
         home_page_candidate = resolve('/')
         self.assertEqual(home_page_candidate.func,
                          home_page,
                          'root url not resolving to homepage')
+
+class FullListPageTest(TestCase):
+
+    def test_full_page_endpoint_loads(self):
+        full_list_page_candidate = resolve('/full-list/')
+        self.assertEqual(full_list_page_candidate.func,
+                         full_list_page,
+                         'Full List page not loading right endpoint')
+
+    def test_full_db_contents_served(self):
+        """
+        Test that contents of db are being correctly served
+        """
+        Card.objects.create(
+            name='test card 1',
+            type='test type',
+            cost=13,
+            treasure=7,
+            victory_points=7,
+            actions='None'
+        )
+        Card.objects.create(
+            name='test card 2',
+            type='test type',
+            cost=13,
+            treasure=7,
+            victory_points=7,
+            actions='None'
+        )
+
+        response = self.client.get(
+            '/full-list/'
+        )
+
+        parser = HTMLParser.HTMLParser()
+        response_content = parser.unescape(
+            response.content
+        )
+        cardDb_contents = serializers.serialize(
+            'json',
+            Card.objects.all()
+        )
+
+        self.assertEqual(str(response_content).strip(), str(cardDb_contents))
--- daleria_archives/views.py
+++ daleria_archives/views.py
@@ -2,9 +2,8 @@ from django.core import serializers
 from django.shortcuts import render
 from daleria_archives.models import Card
 
-def full_list(request):
+def full_list_page(request):
     cards = serializers.serialize("json",Card.objects.all())
-
     return render(request, 'full-list.html', {'cards': cards})
 
 
